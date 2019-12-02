05b4e221600b3a0c50e910ff49cde1b99a86d376
cetv <ctiszav@gmail.com>
2019-12-01 21:20:21 -0500

    new

---
+++ src/main/java/cetv/desafio3/desafio3/controller/Desafio3Controller.java
@@ -0,0 +1,15 @@
+package cetv.desafio3.desafio3.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Desafio3Controller {
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String Test()  {
+
+        return "hello";
+    }
+
+}
