d8f69808b74463be8e0ee86f0f8806c6711d28fd
carlos <ctiszav@gmail.com>
2019-12-01 23:21:41 -0500

    test

---
+++ src/main/java/cetv/desafio3/desafio3/services/TestService.java
@@ -0,0 +1,16 @@
+package cetv.desafio3.desafio3.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String test1(){
+        return "test1";
+    }
+
+    public String test2(){
+        return "test2";
+    }
+
+}
--- src/test/java/cetv/desafio3/desafio3/Desafio3ApplicationTests.java
+++ src/test/java/cetv/desafio3/desafio3/Desafio3ApplicationTests.java
@@ -1,13 +1,28 @@
 package cetv.desafio3.desafio3;
 
+import cetv.desafio3.desafio3.services.TestService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Desafio3ApplicationTests {
 
+	@Autowired
+	TestService testService;
+
 	@Test
-	void contextLoads() {
+	void test1Test() {
+		assertEquals("test1",testService.test1());
+	}
+
+
+	@Test
+	void test2Test() {
+		assertEquals("test2",testService.test2());
 	}
 
 }
